c7d7d91d392e55fb59d54e236965a19c597672f8
Кирилл Милохин <milokhin.ki@reliab.tech>
2024-02-18 18:56:43 +0500

    Добавил компонент кнопки

--- src/components/BaseButton/BaseButton.tsx
+++ src/components/BaseButton/BaseButton.tsx
@@ -1,11 +1,19 @@
-import './style.css';
 import cn from 'classnames';
+export interface BaseButtonProps {
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  children?: React.ReactNode;
+  disabled?: boolean;
+  type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+  className?: string;
+  typeStyle?: 'dark' | 'secondary';
+}
 
-export const BaseButton = (props: any) => {
+export const BaseButton = (props: BaseButtonProps) => {
   return (
     <button
+      onClick={(e) => props.onClick && props.onClick(e)}
       type={props.type || 'button'}
-      onClick={(e) => props.onClick && props.onClick(e)} // Обрабатываем событие нажатия
+      disabled={props.disabled}
       className={cn(
         'BaseButton',
         props.typeStyle === 'dark' ? 'BaseButton--dark' : 'BaseButton--default', // Получаем стиль кнопки
@@ -16,8 +24,3 @@ export const BaseButton = (props: any) => {
     </button>
   );
 };
-
-// typeStyle
-// type
-// onClick
-// children
