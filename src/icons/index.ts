b559c7fa29ae0e054245cc3b857fc07a000d2e5e
Fedor Drozdov <31051998fd@gmail.com>
2024-02-19 03:14:10 +0400

    Базовая реализация компонента "Регистрации" +добавлена иконка подсказки.

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as Logo } from './Logo.svg';
 export { ReactComponent as IconHide } from './IconHidePassword.svg';
 export { ReactComponent as IconShow } from './IconShowPassword.svg';
+export { ReactComponent as IconHint } from './IconHint.svg';
--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
-
+import { BaseInput } from '../../components/BaseComponents/BaseInput';
+import { BaseButton } from '../../components/BaseButton';
+import { BaseWrapperForm } from '../../components/BaseComponents/BaseWrapperForm';
+import { PasswordInput } from '../../components/PasswordInput';
+import { IconHint } from '../../icons';
+import './style.css';
 export const RegistrationPage = () => {
-  return <span>RegistrationPage</span>;
+  return (
+    <BaseWrapperForm className="RegForm">
+      <form className="RegForm__form">
+        <h1 className="RegForm__title">Регистрация</h1>
+        <BaseInput
+          className="RegForm__form_input"
+          value=""
+          label="Имя"
+        ></BaseInput>
+        <BaseInput
+          className="RegForm__form_input"
+          value=""
+          label="Фамилия"
+        ></BaseInput>
+        <PasswordInput
+          className="RegForm__form_input"
+          value=""
+          label="Пароль"
+          OuterComponent={<IconHint className="test" />}
+        ></PasswordInput>
+        <BaseInput
+          className="RegForm__form_input"
+          value=""
+          label="E-mail (корпоративный)"
+          plaseholder="example@reliab.tech"
+        ></BaseInput>
+        <BaseButton className="RegForm__buttonSubmit" typeStyle="dark">
+          Зарегистрироваться
+        </BaseButton>
+      </form>
+    </BaseWrapperForm>
+  );
 };
