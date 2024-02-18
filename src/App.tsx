f1c6c91e6046156c9c125154549de4684da3cf51
Vadim Kogol <vadim.kogol@irlix.ru>
2024-02-18 17:35:27 +0400

    Добавлен роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './router';
+
 function App() {
-  return null;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/ResetPasswordPage/ResetPasswordPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ResetPasswordPage = () => {
+  return <span>ResetPasswordPage</span>;
+};
