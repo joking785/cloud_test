a1e1271dd63a28c97689c17d50bb0dd9a0b7e687
Кирилл Милохин <milokhin.ki@reliab.tech>
2024-02-19 18:59:37 +0500

    Реализация страницы восстановления пароля

--- src/pages/ResetPasswordPage/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
-
+import { BaseInput } from '../../components/BaseComponents/BaseInput';
+import { BaseButton } from '../../components/BaseButton';
+import { BaseWrapperForm } from '../../components/BaseComponents/BaseWrapperForm';
+import { PasswordInput } from '../../components/PasswordInput';
+import { IconHint } from '../../icons';
+import './style.css';
 export const ResetPasswordPage = () => {
-  return <span>ResetPasswordPage</span>;
+  return (
+    <BaseWrapperForm className="ResetForm">
+      <form className="ResetForm__form">
+        <h1 className="ResetForm__title">Восстановление пароля</h1>
+        <BaseInput className="RegForm__form_input" value="" label="Имя" />
+        <BaseInput className="RegForm__form_input" value="" label="Фамилия" />
+        <PasswordInput
+          className="RegForm__form_input"
+          value=""
+          label="Пароль"
+          OuterComponent={<IconHint className="RegForm__form_outerIcon" />}
+        />
+        <BaseInput
+          className="RegForm__form_input"
+          value=""
+          label="E-mail (корпоративный)"
+          plaseholder="example@reliab.tech"
+        />
+        <BaseButton className="RegForm__buttonSubmit" typeStyle="dark">
+          Зарегистрироваться
+        </BaseButton>
+      </form>
+    </BaseWrapperForm>
+  );
 };
